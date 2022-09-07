94f006125506d2a998290d808f2ae2c2ef669931
dsapuohgasd <dima.isa01@gmail.com>
2022-09-07 12:01:27 +0300

    Tests09.07

--- TestProject/AppTest.cs
+++ TestProject/AppTest.cs
@@ -15,20 +15,69 @@ namespace TestProject
         }
 
         [TestMethod]//Парсим симыолы в числа
-        public void RomanNumberParse()
+        public void RomanNumberParse1Digit()
         {
-            Assert.AreEqual(RomanNumber.Parse("I"),1,"I==1");
-            Assert.AreEqual(RomanNumber.Parse("IV"), 4, "IV==4");
-            Assert.AreEqual(RomanNumber.Parse("XV"), 15, "XV==15");
-            Assert.AreEqual(RomanNumber.Parse("XXX"), 30, "XXX==30");
-            Assert.AreEqual(RomanNumber.Parse("CM"), 900, "CM==900");
-            Assert.AreEqual(RomanNumber.Parse("MCMXCIX"), 1999, "MCMXCIX==1999");
-            Assert.AreEqual(RomanNumber.Parse("CD"), 400, "CD==400");
-            Assert.AreEqual(RomanNumber.Parse("LV"), 55, "LV==55");
-            Assert.AreEqual(RomanNumber.Parse("XL"), 40, "XL==40");
+
+            Assert.AreEqual(1, RomanNumber.Parse("I"));
+            Assert.AreEqual(5, RomanNumber.Parse("V"));
+            Assert.AreEqual(10, RomanNumber.Parse("X"));
+            Assert.AreEqual(50, RomanNumber.Parse("L"));
+            Assert.AreEqual(100, RomanNumber.Parse("C"));
+            Assert.AreEqual(500, RomanNumber.Parse("D"));
+            Assert.AreEqual(1000, RomanNumber.Parse("M"));
+
+        }
+        [TestMethod]
+        public void RomanNumberParse2Digit()
+        {
+            Assert.AreEqual(4, RomanNumber.Parse("IV"));
+            Assert.AreEqual(15, RomanNumber.Parse("XV"));
+            Assert.AreEqual(900, RomanNumber.Parse("CM"));
+            Assert.AreEqual(400, RomanNumber.Parse("CD"));
+            Assert.AreEqual(55, RomanNumber.Parse("LV"));
+            Assert.AreEqual(40, RomanNumber.Parse("XL"));
+        }
+        [TestMethod]
+        public void RomanNumberParse3MoreDigit()
+        {
+            Assert.AreEqual(30, RomanNumber.Parse("XXX"));
+            Assert.AreEqual(401, RomanNumber.Parse("CDI"));
+            Assert.AreEqual(1999, RomanNumber.Parse("MCMXCIX"));
+        }
+
+        [TestMethod]
+        public void RomanNumberParseInvalidDigits()
+        {
+            //Assert.AreEqual(30, RomanNumber.Parse("XXA"));
+
+            var exc = Assert.ThrowsException<ArgumentException>(() => { RomanNumber.Parse("XXA"); }); //зберігаємо виключення, що виникає при тестуванні
+            var exp = new ArgumentException("Invalid char A"); // очікуване виключення з попереднього тесту
+            Assert.AreEqual(exp.Message, exc.Message);
+
+            var exc1 = Assert.ThrowsException<ArgumentException>(() => { RomanNumber.Parse("XXJ"); }); //зберігаємо виключення, що виникає при тестуванні
+            var exp1 = new ArgumentException("Invalid char J"); // очікуване виключення з попереднього тесту
+            Assert.AreEqual(exp.Message, exc.Message);
+
+            var exc2 = Assert.ThrowsException<ArgumentException>(() => { RomanNumber.Parse("XXK"); }); //зберігаємо виключення, що виникає при тестуванні
+            var exp2 = new ArgumentException("Invalid char K"); // очікуване виключення з попереднього тесту
+            Assert.AreEqual(exp.Message, exc.Message);
+
+            var exc3 = Assert.ThrowsException<ArgumentException>(() => { RomanNumber.Parse("X X"); }); //зберігаємо виключення, що виникає при тестуванні
+            var exp3 = new ArgumentException("Invalid char"); // очікуване виключення з попереднього тесту
+            Assert.IsTrue(
+                Assert.ThrowsException<ArgumentException>(() => { RomanNumber.Parse("X X"); }).Message.StartsWith("Invalid char"));
 
         }
 
+        [TestMethod]
+        public void RomanNumberParseEmpty()
+        {
+            //вимагати щоб не призводило до виключення
+            //argexc з повідомленням empty str not allow
+            Assert.AreEqual("Empty string not allowed", Assert.ThrowsException<ArgumentException>(()=>RomanNumber.Parse("")).Message);
+
+            Assert.ThrowsException<ArgumentException>(() => RomanNumber.Parse(null!));
+        }
     }
 }
 
